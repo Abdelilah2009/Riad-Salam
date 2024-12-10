80ccc28cb17911de199a2a491608d179b3785894
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2024-12-10 23:43:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,91 @@ jsArrow.onclick = function() {
 
 
 
+
+const navbar=document.getElementById('navbar')
+
+window.addEventListener('scroll',()=>{
+    if (window.scrollY>100) {
+        navbar.classList.add('active')
+    }else{
+        navbar.classList.remove('active')
+        
+    }
+
+})
+
+
+
+// Background header
+
+
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#3e8da8",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "repulse": {
+                "distance": 100,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            }
+        }
+    },
+    "retina_detect": true
+});
+Expliquer
