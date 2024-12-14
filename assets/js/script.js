37bd7b8a481168644f4a7a2511a84224985ac07c
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2024-12-14 23:55:48 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,137 @@
+// LOADER AND BTN LOAD MORE
+
+let cards = document.querySelectorAll(".card");
+let loadMoreButton = document.querySelector(".load-more-button");
+let loader = document.querySelector(".loader");
+let visibleCount = 3;
+
+loadMoreButton.addEventListener("click", () => {
+  loader.style.display = "block"; 
+  loadMoreButton.style.display = "none"; 
+
+  setTimeout(() => {
+    loader.style.display = "none"; 
+    loadMoreButton.style.display = "block"; 
+
+    // Show next 3 cards
+    let nextCards = Array.from(cards).slice(visibleCount, visibleCount + 3);
+    nextCards.forEach((card) => card.classList.add("visible"));
+    visibleCount += 3;
+
+    // Hide button if all cards are visible
+    if (visibleCount >= cards.length) {
+      loadMoreButton.style.display = "none";
+    }
+  }, 1100);
+});
+
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+};
+// let moreArrow = document.querySelector(".more-arrow");
+// moreArrow.onclick = function () {
+//   navLinks.classList.toggle("show2");
+// };
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+};
+
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove("active");
+  }
+});
+
+// Background header
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+    },
+    size: {
+      value: 3,
+      random: true,
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#3e8da8",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+    },
+  },
+  retina_detect: true,
+});
+Expliquer;
+
+
