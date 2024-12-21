3f9786a4fff79cd8ff2f140b5acd81c21c8750e4
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2024-12-21 19:10:10 +0100

    Add files via upload

---
+++ assets/js/contactFR.js
@@ -0,0 +1,35 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_ewhvbj8",
+      "template_hdg2o2i",
+      "#contact-form",
+      "bvntG-oaLC9_LOnwA"
+    )
+    .then(
+      () => {
+        // Show send message
+        contactMessage.textContent = "Message envoyé avec succès ✅";
+
+        // Remove message after five second
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 3000);
+
+        // comment
+        contactForm.reset();
+      },() => {
+        // Show error message
+        contactMessage.textContent = "Message non envoyé (erreur de service) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
