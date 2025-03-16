3e45948bdd6fc2f26591038f2fea3f7029357936
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2025-03-16 12:46:28 +0000

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,9 +1,4 @@
 // LOADER
-
-window.addEventListener("DOMContentLoaded", () => {
-
-  setTimeout(() => {
-
-    document.body.classList.replace("load-before", "load-after");
-  }, 3500);
+window.addEventListener("load", () => {
+  document.body.classList.replace("load-before", "load-after");
 });
