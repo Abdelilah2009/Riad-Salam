01ea30bb4b498cfd284eb5d75921145f4bfd6654
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2025-03-16 12:45:29 +0000

    Add files via upload

---
+++ fr/club/js/club.js
@@ -0,0 +1,44 @@
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+};
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+  navLinks.classList.toggle("show2");
+};
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+};
+
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove("active");
+  }
+});
+
+// Back to top
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 150) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
---
+++ fr/club/js/loader.js
@@ -0,0 +1,9 @@
+// LOADER
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  setTimeout(() => {
+
+    document.body.classList.replace("load-before", "load-after");
+  }, 3500);
+});
